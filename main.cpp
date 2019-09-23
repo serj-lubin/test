fb0d5a42b757c7c06afe01662ef5dcdaa9bb7aaa
serj-lubin <s.yu.lubin@gmail.com>
2019-09-23 15:37:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <unistd.h>
 using namespace std;
 
 int main() 
 {
-	    cout << "Hello, World!\n";
-	        return 0;
+while(true){
+    //run this code
+sleep(5); 	
+}
+
 }
